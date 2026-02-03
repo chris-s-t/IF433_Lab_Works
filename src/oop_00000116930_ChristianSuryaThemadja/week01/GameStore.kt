1d174a310798794fae278a9e875771208084e2ec
ChrisRunners <chrisrunnners@gmail.com>
2026-02-03 16:40:04 +0700

    week01 (tugas 2) implement receipt printing

--- src/oop_00000116930_ChristianSuryaThemadja/week01/GameStore.kt
+++ src/oop_00000116930_ChristianSuryaThemadja/week01/GameStore.kt
@@ -3,6 +3,12 @@ package oop_00000116930_ChristianSuryaThemadja.week01
 fun main() {
     val gameTitle: String = "Game-Gamean"
     val price: Int = 525000
+    val finalPrice: Int = calculateDiscount(price)
+
+    printReceipt(gameTitle, price, finalPrice)
+
 }
 
 fun calculateDiscount(price: Int): Int = if (price>500000) return (price*0.8).toInt() else return (price*0.9).toInt()
+
+fun printReceipt(title: String, price: Int, finalPrice: Int) = println("Game dengan judul $title seharga $price diskon menjadi $finalPrice")
