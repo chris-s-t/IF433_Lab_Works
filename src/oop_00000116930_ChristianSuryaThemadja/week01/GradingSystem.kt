1592750d682122524c81d6bc82313aaa50c9e823
ChrisRunners <chrisrunnners@gmail.com>
2026-02-03 15:23:09 +0700

    week01: initial setup and procedural code

--- src/oop_00000116930_ChristianSuryaThemadja/week01/GradingSystem.kt
+++ src/oop_00000116930_ChristianSuryaThemadja/week01/GradingSystem.kt
@@ -1,2 +1,8 @@
 package oop_00000116930_ChristianSuryaThemadja.week01
 
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    println("Nama: " + name + ", Nilai: " + score)
+}
