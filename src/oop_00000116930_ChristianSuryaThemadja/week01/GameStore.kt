2d89690d198eb4767713577dcc7433669a398bce
ChrisRunners <chrisrunnners@gmail.com>
2026-02-03 16:39:32 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000116930_ChristianSuryaThemadja/week01/GameStore.kt
+++ src/oop_00000116930_ChristianSuryaThemadja/week01/GameStore.kt
@@ -1,7 +1,8 @@
 package oop_00000116930_ChristianSuryaThemadja.week01
 
 fun main() {
-    val gameTitle: String
-    val price: Int
-
+    val gameTitle: String = "Game-Gamean"
+    val price: Int = 525000
 }
+
+fun calculateDiscount(price: Int): Int = if (price>500000) return (price*0.8).toInt() else return (price*0.9).toInt()
