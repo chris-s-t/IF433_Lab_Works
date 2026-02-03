48320b7f6d9c4f8370d3bacb42d76e7a56184ce9
ChrisRunners <chrisrunnners@gmail.com>
2026-02-03 15:41:43 +0700

    week01: add calculateStatus function

--- src/oop_00000116930_ChristianSuryaThemadja/week01/GradingSystem.kt
+++ src/oop_00000116930_ChristianSuryaThemadja/week01/GradingSystem.kt
@@ -1,5 +1,7 @@
 package oop_00000116930_ChristianSuryaThemadja.week01
 
+fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
+
 fun main() {
     val name: String = "John Thor"
     val score: Int = 80
@@ -14,4 +16,5 @@ fun main() {
     }
 
     println("Grade kamu: $grade")
+    println("Status: ${calculateStatus(score)}")
 }
