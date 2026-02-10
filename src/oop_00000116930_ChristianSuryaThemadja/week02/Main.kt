67a852c68fd155de65e1cd6c2f056dd4ed11a5cc
Chris-S-T <chrisrunnners@gmail.com>
2026-02-10 23:26:43 +0700

    week02: (task 2) implement the game

--- src/oop_00000116930_ChristianSuryaThemadja/week02/Main.kt
+++ src/oop_00000116930_ChristianSuryaThemadja/week02/Main.kt
@@ -5,9 +5,10 @@ import java.util.Scanner
 fun main() {
     val scanner = Scanner(System.`in`)
 
-    println("--- Pilih API ---")
+    println("--- Pilih API/GAME ---")
     println("1. API PMB UMN")
     println("2. API LIBRARY UMN")
+    println("3. GAME MINI RPG BATTLE")
 
     val api = scanner.nextInt()
     scanner.nextLine()
@@ -66,6 +67,54 @@ fun main() {
 
         val loan = Loan(bookTitle, borrower, loanDuration)
         println("Buku dengan judul $bookTitle yang dipinjam oleh $borrower selama $loanDuration hari terkena denda sebesar ${loan.calculateFine()} Rupiah.")
+    } else if (api == 3) {
+        println("--- GAME MINI RPG BATTLE ---")
+
+        print("Masukkan Nama Hero: ")
+        val name  = scanner.nextLine()
+
+        print("Masukkan Damage Hero: ")
+        val baseDamage = scanner.nextInt()
+        scanner.nextLine()
+
+        var enemyHP = 100
+        val hero = Hero(name = name, baseDamage = baseDamage)
+
+        while (hero.isAlive() && enemyHP > 0) {
+            println("Pilih Aksi: ")
+            println("1. Serang")
+            println("2. Kabur")
+
+            val pilihan = scanner.nextInt()
+
+            scanner.nextLine()
+
+            if (pilihan == 1) {
+                enemyHP -= baseDamage
+                hero.attack(targetName = "Enemy")
+                if (enemyHP < 0) enemyHP = 0
+                println("Enemy HP: ${enemyHP + baseDamage} > $enemyHP (-${baseDamage})")
+                if (enemyHP > 0) {
+                    val enemyDamage = (10..20).random()
+                    hero.takeDamage(enemyDamage)
+                    println("Musuh menyerang balik! HP Hero: ${hero.hp + enemyDamage} > ${hero.hp} (-${enemyDamage})")
+                }
+            } else if (pilihan == 2) {
+                println("Hero Kabur!")
+                break
+            } else {
+                println("Pilihan ngawur, aksi tidak terdefinisi oleh Game Master!")
+                break
+            }
+        }
+
+        if (hero.hp > enemyHP) {
+            println("Hero $name memenangkan pertarungan! Sisa HP: ${hero.hp}")
+        } else if (hero.hp < enemyHP) {
+            println("Enemy memenangkan pertarungan! Sisa HP $enemyHP")
+        } else {
+            println("Gak ada yang menang! Tamat kan saja lah...")
+        }
     } else {
         print("Pilihan ngawur, masuk api batal!")
     }
