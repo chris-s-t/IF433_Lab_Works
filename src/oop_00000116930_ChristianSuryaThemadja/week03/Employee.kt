b3bef8c727f33475db60be20c46f7092e5f90203
ChrisRunners <chrisrunnners@gmail.com>
2026-02-20 14:54:15 +0700

    week03: add private property

--- src/oop_00000116930_ChristianSuryaThemadja/week03/Employee.kt
+++ src/oop_00000116930_ChristianSuryaThemadja/week03/Employee.kt
@@ -1,6 +1,16 @@
 package oop_00000116930_ChristianSuryaThemadja.week03
 
 class Employee(val name: String) {
+    private var performanceRating: Int = 3
+
+    fun increasePerformance() {
+        performanceRating++
+        println("Kinerja $name meningkat! Rating: $performanceRating")
+    }
+
+    fun printStatus() {
+        println("Karyawan: $name, Rating: $performanceRating")
+    }
     var salary: Int = 0
         set(value) {
             if (value < 0) {
