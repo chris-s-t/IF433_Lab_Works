49517c01aa60f334827624d0afb4519e2618446b
ChrisRunners <chrisrunnners@gmail.com>
2026-03-10 15:45:39 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_00000116930_ChristianSuryaThemadja/week06/SmartHomeHub.kt
@@ -0,0 +1,17 @@
+package oop_00000116930_ChristianSuryaThemadja.week06
+
+class SmartHomeHub() {
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+}
