a15c86c923c29df7b68f2bd8fb8d81d442f1d252
ChrisRunners <chrisrunnners@gmail.com>
2026-04-21 15:41:08 +0700

    week09: (task) print top performers using forEach

--- src/oop_00000116930_ChristianSuryaThemadja/week09/TradingDashboard.kt
+++ src/oop_00000116930_ChristianSuryaThemadja/week09/TradingDashboard.kt
@@ -13,10 +13,12 @@ fun main() {
     val closedTrades = tradeHistory.filter { it.status == "CLOSED" }
     val winningTrades = closedTrades.filter { it.roe > 0 }
     val losingTrades = closedTrades.filter { it.roe <= 0 }
-    val topPerformersString = winningTrades.sortedByDescending { it.roe }.map { println("WIN [${it.pair} - ${it.position}]: +${it.roe} ROE (Lev: ${it.leverage}x)") }
-    val worstPerformersString = losingTrades.sortedByDescending { it.roe }.map { println("LOSS [${it.pair} - ${it.position}]: +${it.roe} ROE (Lev: ${it.leverage}x)") }
+    val topPerformersString = winningTrades.sortedByDescending { it.roe }.map { "WIN [${it.pair} - ${it.position}]: +${it.roe} ROE (Lev: ${it.leverage}x)" }
+    val worstPerformersString = losingTrades.sortedByDescending { it.roe }.map { "LOSS [${it.pair} - ${it.position}]: +${it.roe} ROE (Lev: ${it.leverage}x)" }
     val uniquePairs = tradeHistory
         .map { it.pair }
         .toSet()
 
+    println("=== CRYPTO TRADING DASHBOARD ===")
+    topPerformersString.forEach { println(it) }
 }
