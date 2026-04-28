b6091853a953d9d75ec9ac4ff5941d207aabff83
ChrisRunners <chrisrunnners@gmail.com>
2026-04-28 16:17:03 +0700

    week10: (task) populate coin repository

--- src/oop_00000116930_ChristianSuryaThemadja/week10/CryptoDashboard.kt
+++ src/oop_00000116930_ChristianSuryaThemadja/week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_00000116930_ChristianSuryaThemadja.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 5.0))
+    coinRepo.add(Coin("ETH", 20.0))
+    coinRepo.add(Coin("USDT", 500.0))
 }
