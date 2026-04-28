cfa1373a3547a024d3926e76af49956bf93de88d
ChrisRunners <chrisrunnners@gmail.com>
2026-04-28 16:27:42 +0700

    week10: (task) test full generic crypto architecture

--- src/oop_00000116930_ChristianSuryaThemadja/week10/CryptoDashboard.kt
+++ src/oop_00000116930_ChristianSuryaThemadja/week10/CryptoDashboard.kt
@@ -9,7 +9,7 @@ fun main() {
 
     val response = ApiResponse("200 OK", coinRepo.getAll())
 
-    println("Response Setiap Koin")
+    println("=== Response Setiap Koin ===")
     response.data.forEach { Coin ->
         println("Nama: ${Coin.name}, Saldo: ${Coin.balance}")
     }
@@ -18,4 +18,11 @@ fun main() {
     txRepo.add(Transaction("TX1", 250.0))
     txRepo.add(Transaction("TX2", 500.0))
     txRepo.add(Transaction("TX3", 750.0))
+
+    val responseTransaction = ApiResponse("200 OK", txRepo.getAll())
+
+    println("\n=== Response Setiap Transaksi ===")
+    responseTransaction.data.forEach { tx ->
+        println("ID: ${tx.id}, Saldo: ${tx.amount}")
+    }
 }
