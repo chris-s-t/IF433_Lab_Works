3558f51658c8c4042240deb67c7d4772e7714998
ChrisRunners <chrisrunnners@gmail.com>
2026-05-05 12:32:34 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_00000116930_ChristianSuryaThemadja/week11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_00000116930_ChristianSuryaThemadja.week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
